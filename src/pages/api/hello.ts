d2c6a9550e6f1e120f95fa949d37b20726b3ed28
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2021-01-09 21:16:28 -0300

    refactor: sync project with next.js default template

---
+++ src/pages/api/hello.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  res.statusCode = 200;
+  res.json({ name: `John Doe` });
+};
