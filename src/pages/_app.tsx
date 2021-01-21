877a3ec3ec0c99fafd9b60a3c40bb3b6efe4bdb7
Arafat Zahan <kuasha420@gmail.com>
2021-01-21 23:41:30 +0600

    chore(tsc): change tsc target to es2015 and enable strict mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/global.css';
+import { AppProps } from 'next/app';
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
