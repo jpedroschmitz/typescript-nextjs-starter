3f142716d342fba7dc71c83134fb596f4d1694cd
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2021-06-16 10:48:48 -0300

    refactor: sync template

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Image from 'next/image';
+
+import styles from '@/styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>TypeScript starter for Next.js</title>
+        <meta
+          name="description"
+          content="TypeScript starter for Next.js that includes all you need to build amazing apps"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -23,12 +29,12 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
+            <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
+            <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
@@ -36,7 +42,7 @@ export default function Home() {
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
+            <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
@@ -44,7 +50,7 @@ export default function Home() {
             href="https://vercel.com/new?utm_source=typescript-nextjs-starter"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h2>Deploy &rarr;</h2>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
@@ -59,7 +65,9 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{` `}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
         </a>
       </footer>
     </div>
