6dc27c6f217caeee1cc2e796fc3c9e7a4c40f392
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2021-06-16 10:48:48 -0300

    refactor: sync template

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,7 +1,13 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.statusCode = 200;
-  res.json({ name: `John Doe` });
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  name: string;
 };
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  res.status(200).json({ name: `John Doe` });
+}
