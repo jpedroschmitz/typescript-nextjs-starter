33ae6c823c87344f73687d65978ecd31a5d6b83e
João Pedro Schmitz <oi@joaopedro.cc>
2022-08-04 21:35:22 -0300

    chore: sync template

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import { AppProps } from 'next/app';
-import '@/styles/global.css';
+import type { AppProps } from 'next/app';
+import '@/styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
