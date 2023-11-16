e84a2e5d64d0f1b1e46dc7927479497c7dadad25
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2023-11-16 19:52:06 -0300

    chore: update prettier config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from 'next';
+
 import './globals.css';
+
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -9,11 +11,7 @@ export const metadata: Metadata = {
   description: `TypeScript starter for Next.js that includes all you need to build amazing apps`,
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
